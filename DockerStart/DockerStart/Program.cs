abc4651fb09ef3c7d617d1329d3b73e4bc3e7c4c
Ales Ban <alesbah04@gmail.com>
2023-03-14 00:36:27 +0300

    create new proj

---
+++ DockerStart/DockerStart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
